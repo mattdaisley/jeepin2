c8b38044948391f48cf02d96b80e45d586a35fef
Matt Daisley <mattdaisley@gmail.com>
2017-02-08 12:15:10 -0700

    update python script to use sense hat

--- core/server/senseHat/sense/get_orientation.py
+++ core/server/senseHat/sense/get_orientation.py
@@ -4,7 +4,7 @@ import time
 import sys
 from random import randint, uniform
 
-# sense = SenseHat()
+sense = SenseHat()
 
 # print(json.dumps( { 'result': sense.get_orientation() } ))
 # print(json.dumps( {'result':{'pitch':'100', 'roll': '200'}} ))
@@ -14,8 +14,9 @@ roll = 0;
 step = 2;
 
 while True:
-    pitch = (pitch + uniform(-step, step))
-    roll = (roll + uniform(-step, step))
-    print(json.dumps( {'result':{'pitch':pitch, 'roll': roll}} ))
+    # pitch = (pitch + uniform(-step, step))
+    # roll = (roll + uniform(-step, step))
+    # print(json.dumps( {'result':{'pitch':pitch, 'roll': roll}} ))
+    print(json.dumps( { 'result': sense.get_orientation() } ))
     sys.stdout.flush()
     time.sleep(.1)
