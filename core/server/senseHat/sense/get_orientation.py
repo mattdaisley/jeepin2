75cac9381e148836a4201a547eae0464eda7aad5
Matt Daisley <mattdaisley@gmail.com>
2017-02-08 12:41:48 -0700

    testing with fake data

--- core/server/senseHat/sense/get_orientation.py
+++ core/server/senseHat/sense/get_orientation.py
@@ -1,10 +1,10 @@
-from sense_hat import SenseHat
+# from sense_hat import SenseHat
 import json
 import time
 import sys
 from random import randint, uniform
 
-sense = SenseHat()
+# sense = SenseHat()
 
 # print(json.dumps( { 'result': sense.get_orientation() } ))
 # print(json.dumps( {'result':{'pitch':'100', 'roll': '200'}} ))
@@ -14,9 +14,9 @@ roll = 0;
 step = 2;
 
 while True:
-    # pitch = (pitch + uniform(-step, step))
-    # roll = (roll + uniform(-step, step))
-    # print(json.dumps( {'result':{'pitch':pitch, 'roll': roll}} ))
-    print(json.dumps( { 'result': sense.get_orientation() } ))
+    pitch = (pitch + uniform(-step, step))
+    roll = (roll + uniform(-step, step))
+    print(json.dumps( {'result':{'pitch':pitch, 'roll': roll}} ))
+    # print(json.dumps( { 'result': sense.get_orientation() } ))
     sys.stdout.flush()
     time.sleep(.1)
