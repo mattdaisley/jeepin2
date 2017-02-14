38f04545a6bd5db0132d54817d13733bd88c9a4e
Matt Daisley <mattdaisley@gmail.com>
2017-02-13 19:03:43 -0700

    change rc.local file permissions

--- setup/setup.sh
+++ setup/setup.sh
@@ -27,6 +27,9 @@ sudo mv /home/pi/jeepin2/setup/pulseaudio.desktop /home/pi/.config/autostart/pul
 
 sudo mv /etc/rc.local /etc/rc.local.deac
 sudo mv /home/pi/jeepin2/setup/rc.local /etc/rc.local
+sudo chown root /etc/rc.local
+sudo chgrp root /etc/rc.local
+sudo chmod 755 rc.local
 
 # sudo echo "Class = 0x200420" >> /etc/bluetooth/main.conf
 
