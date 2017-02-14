f7d005575631fbd26fba7f8c4ed8dfbf31a32430
Matt Daisley <mattdaisley@gmail.com>
2017-02-13 17:54:59 -0700

    updated node install

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@
 sudo apt-get update
 
 #sudo apt-get -y install nodejs npm node-semver
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
+sudo apt-get install -y build-essential python-dev nodejs
 
-#cd jeepin2 
+cd jeepin2 
 
-#sudo npm install 
+sudo npm install 
