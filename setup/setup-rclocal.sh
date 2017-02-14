2ab6c17a86b8866ee22b247508ea02b660cf47f6
Matt Daisley <mattdaisley@gmail.com>
2017-02-13 21:10:58 -0700

    update rclocal setup file

--- setup/setup-rclocal.sh
+++ setup/setup-rclocal.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+echo 'sudo mv /etc/rc.local /etc/rc.local.deac'
 sudo mv /etc/rc.local /etc/rc.local.deac
-sudo mv /home/pi/jeepin2/setup/rc.local /etc/rc.local
+
+echo 'sudo mv /home/pi/jeepin2/setup/rc.local /etc/rc.local'
+sudo cp /home/pi/jeepin2/setup/rc.local /etc/rc.local
+
+echo 'sudo chown root /etc/rc.local'
 sudo chown root /etc/rc.local
+
+echo 'sudo chgrp root /etc/rc.local'
 sudo chgrp root /etc/rc.local
-sudo chmod 755 rc.local
+
+echo 'sudo chmod 755 /etc/rc.local'
+sudo chmod 755 /etc/rc.local
