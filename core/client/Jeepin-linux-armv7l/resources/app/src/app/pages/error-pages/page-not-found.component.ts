4f4ba73683716ba23e6e51240e53a890547f930b
Matt Daisley <mattdaisley@gmail.com>
2017-02-22 00:10:08 -0700

    fix index base

--- core/client/Jeepin-linux-armv7l/resources/app/src/app/pages/error-pages/page-not-found.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  styleUrls: ['./error-pages.scss'],
-  template: '<div class="container"><h2>Page not found</h2></div>'
-})
-
-export class PageNotFoundComponent {}
--- core/client/Jeepin-linux-armv7l/resources/app/src/app/pages/tilt-gauges/gyro.interface.ts
+++
@@ -1,4 +0,0 @@
-export interface Gyro {
-  pitch: number,
-  roll: number
-}
